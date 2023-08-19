90af07d560943996f171ecda3e4f5c613ba5805e
Jon <caraveo@me.com>
2023-08-19 10:13:22 -0600

    Version Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     packages=['yt'],
     package_dir={'yt': 'yt'},
     name='yt-prompt',
-    version='0.0.6',
+    version='0.0.7',
         entry_points = {
         "console_scripts": ['youtube = yt.yt:main']
         },
